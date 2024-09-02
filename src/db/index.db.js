5a86a9a95752d2908ad8fa96cedb5f250462d4ea
d128 <pds02802@gmail.com>
2024-09-02 16:07:28 -0500

    Error

--- src/db/index.db.js
+++ src/db/index.db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import { DB_NAME } from '../constants.js';
 
 
-async function connectDB(){
+const connectDB=async function (){
     try {
         const connectInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${ DB_NAME}`)
         console.log(`\n MongoDb connected !! DB Host ${connectInstance}`)
